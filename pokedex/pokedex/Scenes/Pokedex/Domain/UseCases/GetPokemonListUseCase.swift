73bf0d6cd44a36e465af732401afd9ce8ef12583
Gabriel Ferraz Fontes <brielff@hotmail.com>
2021-07-01 11:24:26 -0300

    Organize get pokemon use case

--- pokedex/pokedex/Scenes/Pokedex/Domain/UseCases/GetPokemonListUseCase.swift
+++ pokedex/pokedex/Scenes/Pokedex/Domain/UseCases/GetPokemonListUseCase.swift
@@ -1,19 +1,26 @@
-import Foundation
-
 enum GetPokemonListError: Error {
     case generic
     case noData
 }
 
 protocol GetPokemonListUseCase {
-
+    func getPokemonList(completion: @escaping (Result<[PokemonEntity], GetPokemonListError>) -> Void)
 }
 
-struct GetPokemonList: GetPokemonListUseCase {
+struct GetPokemonList {
+    let repository: Repository
+}
 
-//    let api = 
-
-    func getPokemonList(completion: @escaping (Result<PokemonEntity, Error>) -> Void) {
-
+extension GetPokemonList: GetPokemonListUseCase {
+    func getPokemonList(completion: @escaping (Result<[PokemonEntity], GetPokemonListError>) -> Void) {
+        repository.fetchPokemons { result in
+            switch result {
+            case .success(let pokemons):
+                print(pokemons)
+                completion(.success(pokemons))
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
