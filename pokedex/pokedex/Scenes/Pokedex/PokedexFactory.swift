40527ce3d1ab8116a84e9befe08bbc200a37c166
Gabriel Ferraz Fontes <brielff@hotmail.com>
2021-07-01 11:22:44 -0300

    Add factory and call view controller

--- pokedex/pokedex/Scenes/Pokedex/PokedexFactory.swift
+++ pokedex/pokedex/Scenes/Pokedex/PokedexFactory.swift
@@ -1,8 +1,12 @@
-//
-//  PokedexFactory.swift
-//  pokedex
-//
-//  Created by Gabriel Fontes on 01/07/21.
-//
+import UIKit
 
-import Foundation
+enum PokedexFactory {
+    static func make() -> UIViewController {
+        let repository: Repository = API()
+        let useCase: GetPokemonListUseCase = GetPokemonList(repository: repository)
+        let viewModel: PokedexViewModeling = PokedexViewModel(getPokemonUseCase: useCase)
+        let viewController = PokedexViewController(viewModel: viewModel)
+
+        return viewController
+    }
+}
