91593bdbe3bc4a84052fb7d377b4dd29abef06db
Gabriel Ferraz Fontes <brielff@hotmail.com>
2021-07-01 15:00:38 -0300

    Refactor at API

--- pokedex/pokedex/Scenes/Pokedex/Data/Network/API.swift
+++ pokedex/pokedex/Scenes/Pokedex/Data/Network/API.swift
@@ -5,55 +5,24 @@ final class API {
 }
 
 private extension API {
-    func fetchPokemonList(completion: @escaping (Result<PokemonListResponse, GetPokemonListError>) -> Void) {
-        let pokemonEndPoint = nextURL ?? "https://pokeapi.co/api/v2/pokemon?limit=51"
-        guard let urlRequest = createURLRequest(url: pokemonEndPoint) else {
-            completion(.failure(.generic))
-            return
-        }
-
-        let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] (data, _, _)  in
-            guard let data = data else {
-                completion(.failure(.noData))
-                return
-            }
-
-            guard let pokemonList = try? JSONDecoder().decode(PokemonListResponse.self, from: data) else {
-                completion(.failure(.generic))
-                return
-            }
-
-            self?.nextURL = pokemonList.next
-            completion(.success(pokemonList))
-        }
-        task.resume()
-    }
-
-    func fetchPokemon(url: String, completion: @escaping (Result<PokemonEntity, GetPokemonListError>) -> Void) {
+    func fetch<T:Decodable>(url: String, completion: @escaping (Result<T, GetPokemonListError>) -> Void) {
         guard let urlRequest = createURLRequest(url: url) else {
             completion(.failure(.generic))
             return
         }
 
-        let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] (data, _, _)  in
-            guard let self = self else {
-                completion(.failure(.generic))
-                return
-            }
-
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, _, _)  in
             guard let data = data else {
                 completion(.failure(.noData))
                 return
             }
 
-            guard let pokemonInfo = try? JSONDecoder().decode(PokemonInfoResponse.self, from: data) else {
+            guard let value = try? JSONDecoder().decode(T.self, from: data) else {
                 completion(.failure(.generic))
                 return
             }
 
-            let pokemonEntity = self.parsePokemonResponseToEntity(pokemonInfo: pokemonInfo)
-
-            completion(.success(pokemonEntity))
+            completion(.success(value))
         }
         task.resume()
     }
@@ -64,6 +33,38 @@ private extension API {
         }
 
         return  URLRequest(url: url)
+    }
+
+    func getPokemonEntity(response: PokemonListResponse, completion: @escaping (Result<[PokemonEntity], GetPokemonListError>) -> Void) {
+        let group = DispatchGroup()
+        var getPokemonListError: GetPokemonListError?
+        var pokemons: [PokemonEntity] = []
+
+        response.results.forEach { [weak self] pokemonResponse in
+            group.enter()
+            self?.fetch(url: pokemonResponse.url) { (pokemonInfoResponse: Result<PokemonInfoResponse, GetPokemonListError>) in
+                switch pokemonInfoResponse {
+                case .success(let pokemonInfo):
+                    guard let pokemonEntity = self?.parsePokemonResponseToEntity(pokemonInfo: pokemonInfo) else {
+                        getPokemonListError = .noData
+                        return
+                    }
+                    pokemons.append(pokemonEntity)
+                    group.leave()
+                case .failure(let error):
+                    getPokemonListError = error
+                    group.leave()
+                }
+            }
+        }
+
+        group.notify(queue: DispatchQueue.global()) {
+            if let error = getPokemonListError {
+                completion(.failure(error))
+            } else {
+                completion(.success(pokemons))
+            }
+        }
     }
 
     func parsePokemonResponseToEntity(pokemonInfo: PokemonInfoResponse) -> PokemonEntity {
@@ -81,29 +82,32 @@ private extension API {
 
 extension API: Repository {
     func fetchPokemons(completion: @escaping (Result<[PokemonEntity], GetPokemonListError>) -> Void) {
+        let pokemonEndPoint = nextURL ?? "https://pokeapi.co/api/v2/pokemon?limit=51"
         let group = DispatchGroup()
-        var pokemons: [PokemonEntity] = []
+        var getPokemonListError: GetPokemonListError?
+        var pokemonListResponse: PokemonListResponse?
 
-        fetchPokemonList { [weak self] result in
+        group.enter()
+        fetch(url: pokemonEndPoint) { [weak self] (result: Result<PokemonListResponse, GetPokemonListError>) in
             switch result {
             case .success(let response):
-                for pokemonResult in response.results {
-                    group.enter()
-                    self?.fetchPokemon(url: pokemonResult.url) { result in
-                        switch result {
-                        case .success(let pokemonEntity):
-                            pokemons.append(pokemonEntity)
-                        case .failure(let error):
-                            print(error)
-                        }
-                        group.leave()
-                    }
-                }
-                group.notify(queue: DispatchQueue.global()) {
-                    completion(.success(pokemons))
-                }
+                pokemonListResponse = response
+                self?.nextURL = response.next
+                group.leave()
             case .failure(let error):
-                print(error)
+                getPokemonListError = error
+                group.leave()
+            }
+        }
+
+        group.notify(queue: DispatchQueue.global()) { [weak self] in
+            if let response = pokemonListResponse {
+                self?.getPokemonEntity(response: response) { (entities: Result<[PokemonEntity], GetPokemonListError>) in
+                    completion(entities)
+                }
+            } else {
+                guard let error = getPokemonListError else { return }
+                completion(.failure(error))
             }
         }
     }
