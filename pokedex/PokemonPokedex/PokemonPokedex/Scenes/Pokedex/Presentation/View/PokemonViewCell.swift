202ebe955eeae8f178e000e8bd05e9fa3178533c
Gabriel Ferraz Fontes <brielff@hotmail.com>
2021-07-30 11:40:21 -0300

    Add screen to pokemon view cell

--- pokedex/PokemonPokedex/PokemonPokedex/Scenes/Pokedex/Presentation/View/PokemonViewCell.swift
+++ pokedex/PokemonPokedex/PokemonPokedex/Scenes/Pokedex/Presentation/View/PokemonViewCell.swift
@@ -9,55 +9,11 @@ final class PokemonViewCell: UICollectionViewCell {
 
     static let identifier = "PokemonCell"
 
-    let defaultImage = "pokemonDefault"
-    let defaultName = "Pikachu"
-    let defaultNumber = "#25"
-
-    let fontName = "Arial"
-
-    private lazy var backgroundSubview: UIView = {
-        let view = UIView(frame: .zero)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .lightGray
-        view.layer.cornerRadius = 5
-
-        return view
-    }()
-
-    private lazy var pokemonImage: UIImageView = {
-        let imageDefault = UIImage(named: defaultImage)
-        let imageView = UIImageView(image: imageDefault)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
-        imageView.setContentHuggingPriority(.required, for: .horizontal)
-
-        return imageView
-    }()
-
-    private lazy var pokemonNameLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = defaultName
-        label.textAlignment = .center
-        label.font = UIFont(name: fontName, size: 16)
-
-        return label
-    }()
-
-    private lazy var pokemonNumberLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = defaultNumber
-        label.textAlignment = .center
-        label.font = UIFont(name: fontName, size: 14)
-
-        return label
-    }()
+    lazy var view = PokemonViewCellScreen()
 
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
-
-        setupScene()
+        addScreen()
     }
 
     required init?(coder: NSCoder) {
@@ -65,49 +21,20 @@ final class PokemonViewCell: UICollectionViewCell {
     }
 }
 
-extension PokemonViewCell: ViewConfiguration {
-    func buildHierarchy() {
-        contentView.addSubview(backgroundSubview)
-        backgroundSubview.addSubview(pokemonImage)
-        backgroundSubview.addSubview(pokemonNameLabel)
-        backgroundSubview.addSubview(pokemonNumberLabel)
-    }
-
-    func addConstraints() {
+extension PokemonViewCell {
+    func addScreen() {
+        backgroundView = view
         NSLayoutConstraint.activate([
-            backgroundSubview.topAnchor.constraint(equalTo: self.contentView.topAnchor),
-            backgroundSubview.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
-            backgroundSubview.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
-            backgroundSubview.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
-        ])
-
-        NSLayoutConstraint.activate([
-            pokemonNumberLabel.heightAnchor.constraint(equalToConstant: 20),
-            pokemonNumberLabel.leadingAnchor.constraint(equalTo: self.backgroundSubview.leadingAnchor),
-            pokemonNumberLabel.bottomAnchor.constraint(equalTo: self.backgroundSubview.bottomAnchor),
-            pokemonNumberLabel.trailingAnchor.constraint(equalTo: self.backgroundSubview.trailingAnchor)
-        ])
-
-        NSLayoutConstraint.activate([
-            pokemonNameLabel.heightAnchor.constraint(equalToConstant: 16),
-            pokemonNameLabel.leadingAnchor.constraint(equalTo: self.backgroundSubview.leadingAnchor),
-            pokemonNameLabel.bottomAnchor.constraint(equalTo: self.pokemonNumberLabel.topAnchor),
-            pokemonNameLabel.trailingAnchor.constraint(equalTo: self.backgroundSubview.trailingAnchor)
-        ])
-
-        NSLayoutConstraint.activate([
-            pokemonImage.topAnchor.constraint(equalTo: self.backgroundSubview.topAnchor),
-            pokemonImage.leadingAnchor.constraint(equalTo: self.backgroundSubview.leadingAnchor),
-            pokemonImage.bottomAnchor.constraint(equalTo: self.pokemonNameLabel.topAnchor),
-            pokemonImage.trailingAnchor.constraint(equalTo: self.backgroundSubview.trailingAnchor)
+            view.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            view.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            view.heightAnchor.constraint(equalTo: contentView.heightAnchor),
+            view.widthAnchor.constraint(equalTo: contentView.widthAnchor)
         ])
     }
 }
 
 extension PokemonViewCell: PokemonViewCellDisplaying {
     func setupCell(with pokemon: PokemonCellModel) {
-        pokemonNameLabel.text = pokemon.name
-        pokemonNumberLabel.text = pokemon.number
-        pokemonImage.image = UIImage(data: pokemon.image)
+        view.setupCell(with: pokemon)
     }
 }
---
+++ pokedex/PokemonPokedex/PokemonPokedex/Scenes/Pokedex/Presentation/View/PokemonViewCellScreen.swift
@@ -0,0 +1,106 @@
+import UIKit
+import UI
+
+final class PokemonViewCellScreen: UIView {
+    let defaultImage = "pokemonDefault"
+    let defaultName = "Pikachu"
+    let defaultNumber = "#25"
+
+    let fontName = "Arial"
+
+    private lazy var backgroundSubview: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .lightGray
+        view.layer.cornerRadius = 5
+
+        return view
+    }()
+
+    private lazy var pokemonImage: UIImageView = {
+        let imageDefault = UIImage(named: defaultImage)
+        let imageView = UIImageView(image: imageDefault)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.setContentHuggingPriority(.required, for: .horizontal)
+
+        return imageView
+    }()
+
+    private lazy var pokemonNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = defaultName
+        label.textAlignment = .center
+        label.font = UIFont(name: fontName, size: 16)
+
+        return label
+    }()
+
+    private lazy var pokemonNumberLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = defaultNumber
+        label.textAlignment = .center
+        label.font = UIFont(name: fontName, size: 14)
+
+        return label
+    }()
+
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        
+        setupScene()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PokemonViewCellScreen: ViewConfiguration {
+    func buildHierarchy() {
+        addSubview(backgroundSubview)
+        backgroundSubview.addSubview(pokemonImage)
+        backgroundSubview.addSubview(pokemonNameLabel)
+        backgroundSubview.addSubview(pokemonNumberLabel)
+    }
+
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundSubview.topAnchor.constraint(equalTo: topAnchor),
+            backgroundSubview.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundSubview.bottomAnchor.constraint(equalTo: bottomAnchor),
+            backgroundSubview.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            pokemonNumberLabel.heightAnchor.constraint(equalToConstant: 20),
+            pokemonNumberLabel.leadingAnchor.constraint(equalTo: self.backgroundSubview.leadingAnchor),
+            pokemonNumberLabel.bottomAnchor.constraint(equalTo: self.backgroundSubview.bottomAnchor),
+            pokemonNumberLabel.trailingAnchor.constraint(equalTo: self.backgroundSubview.trailingAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            pokemonNameLabel.heightAnchor.constraint(equalToConstant: 16),
+            pokemonNameLabel.leadingAnchor.constraint(equalTo: self.backgroundSubview.leadingAnchor),
+            pokemonNameLabel.bottomAnchor.constraint(equalTo: self.pokemonNumberLabel.topAnchor),
+            pokemonNameLabel.trailingAnchor.constraint(equalTo: self.backgroundSubview.trailingAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            pokemonImage.topAnchor.constraint(equalTo: self.backgroundSubview.topAnchor),
+            pokemonImage.leadingAnchor.constraint(equalTo: self.backgroundSubview.leadingAnchor),
+            pokemonImage.bottomAnchor.constraint(equalTo: self.pokemonNameLabel.topAnchor),
+            pokemonImage.trailingAnchor.constraint(equalTo: self.backgroundSubview.trailingAnchor)
+        ])
+    }
+}
+
+extension PokemonViewCellScreen {
+    func setupCell(with pokemon: PokemonCellModel) {
+        pokemonNameLabel.text = pokemon.name
+        pokemonNumberLabel.text = pokemon.number
+        pokemonImage.image = UIImage(data: pokemon.image)
+    }
+}
