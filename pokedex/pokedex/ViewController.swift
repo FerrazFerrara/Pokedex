cc6c0e0c7adf9d42476f2ff503c1edc343ccbc3d
Gabriel Ferraz Fontes <brielff@hotmail.com>
2021-08-30 23:20:34 -0300

    Remove useless code

--- pokedex/pokedex/ViewController.swift
+++ pokedex/pokedex/ViewController.swift
@@ -8,17 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    lazy var viewTest = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addSubview(viewTest)
-        viewTest.translatesAutoresizingMaskIntoConstraints = false
-        viewTest.backgroundColor = .red
     }
-
-
 }
 
