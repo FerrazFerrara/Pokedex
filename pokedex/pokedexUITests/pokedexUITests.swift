f62d7dea1e9c5340aca5c1459cb572ef39f43831
Gabriel Ferraz Fontes <brielff@hotmail.com>
2021-09-15 15:21:30 -0300

    Add snapshot tests

--- pokedex/pokedexUITests/pokedexUITests.swift
+++ pokedex/pokedexUITests/pokedexUITests.swift
@@ -2,16 +2,30 @@ import XCTest
 
 class pokedexUITests: XCTestCase {
 
+    lazy var app = XCUIApplication()
+
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
 
     func testCellsExistence_WhenLaunchApp() throws {
-        let app = XCUIApplication()
         app.launch()
 
         _ = app.collectionViews.cells.element(boundBy: 0).waitForExistence(timeout: 20)
 
         XCTAssert(app.collectionViews.cells.element(boundBy: 2).exists)
+        takeScreenshot(name: "Pokedex")
+    }
+
+    func takeScreenshot(name: String) {
+        let fullScreenshot = XCUIScreen.main.screenshot()
+
+        let attachment = XCTAttachment(uniformTypeIdentifier: "public.png", name: "\(UIDevice.current.model)-\(name).png",
+                                       payload: fullScreenshot.pngRepresentation,
+                                       userInfo: nil)
+
+        attachment.lifetime = .keepAlways
+
+        add(attachment)
     }
 }
